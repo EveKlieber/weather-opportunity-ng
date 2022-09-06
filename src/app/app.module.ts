1a6b09106d75029922f8ebcb0176c24f876e849a
EveKlieber <Kliebereveline@gmail.com>
2022-09-06 17:03:08 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { WeatherResultComponent } from './weather-result/weather-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemperatureComponent,
+    WeatherResultComponent
   ],
   imports: [
     BrowserModule,
